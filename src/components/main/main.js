5a97ab1f63b7f882d0ae26a95707c488772f3886
Brevan Bender <bcaybender@gmail.com>
2021-12-08 10:07:29 -0800

    finished main.js ugly but functional

---
+++ src/components/main/main.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from 'react';
+import './main.css';
+import FlagCard from '../flagcard/flagcard';
+import { getFlags } from '../../services/getflags';
+
+export default function Main() {
+  const [flags, setFlags] = useState([]);
+  const [query, setQuery] = useState('');
+  const [continent, setContinent] = useState('all');
+  const [order, setOrder] = useState('default');
+
+  const [wait, setWait] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getFlags();
+      setFlags(data);
+      setWait(false);
+    };
+    fetchData();
+  }, []);
+
+  const filterSortFlags = () => {
+    return flags
+      .sort((a, b) => {
+        if (order === 'asc') {
+          if (a.name < b.name) {
+            return -1;
+          }
+          if (a.name > b.name) {
+            return 1;
+          }
+
+          return 0;
+        } else if (order === 'dsc') {
+          if (a.name > b.name) {
+            return -1;
+          }
+          if (a.name < b.name) {
+            return 1;
+          }
+
+          return 0;
+        }
+      })
+      .filter((flag) => {
+        return flag.name.includes(query) && (flag.continent === continent || continent === 'all');
+      });
+  };
+
+  if (wait) return <h1>LOADING</h1>;
+
+  return (
+    <>
+      <div>
+        <label className="options">
+          Search for Flag:
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => {
+              setQuery(e.target.value);
+            }}
+          />
+        </label>
+
+        <label className="options">
+          Filter by Continent:
+          <select
+            value={continent}
+            onChange={(e) => {
+              setContinent(e.target.value);
+            }}
+          >
+            <option value="all">All</option>
+            <option value="Asia">Asia</option>
+            <option value="Africa">Africa</option>
+            <option value="Antarctica">Antarctica</option>
+            <option value="Europe">Europe</option>
+            <option value="North America">North America</option>
+            <option value="South America">South America</option>
+            <option value="Oceania">Oceania</option>
+          </select>
+        </label>
+
+        <label className="options">
+          Sort Alphabetically:
+          <select
+            value={order}
+            onChange={(e) => {
+              setOrder(e.target.value);
+            }}
+          >
+            <option value="default">Default</option>
+            <option value="asc">A-Z</option>
+            <option value="dsc">Z-A</option>
+          </select>
+        </label>
+      </div>
+
+      <div className="flag-display">
+        {filterSortFlags().map((flag) => {
+          return <FlagCard key={flag.name} {...flag} />;
+        })}
+      </div>
+    </>
+  );
+}
